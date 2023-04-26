8bb13237964a570b691f1a23691b2f2f3fc5a922
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-26 15:33:48 -0300

    :sparkles: feat: Passo 3!

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,5 @@
-sap.ui.define([], function () {
+sap.ui.define(["sap/m/Text"], function (Text) {
   "use strict";
 
-  alert("UI5 is ready");
+  new Text({ text: "Hello World" }).placeAt("content");
 });
