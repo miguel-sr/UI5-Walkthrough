188d5dc2f7e869ad1e9059ac369652d6a9d46add
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-27 12:16:18 -0300

    :sparkles: feat: Passo 10!

--- webapp/Component.js
+++ webapp/Component.js
@@ -10,12 +10,7 @@ sap.ui.define(
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "sap.ui.demo.walkthrough.view.App",
-          type: "XML",
-          /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-          id: "app",
-        },
+        manifest: "json",
       },
       init: function () {
         // call the init function of the parent
