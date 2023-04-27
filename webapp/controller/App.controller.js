c84a01eb2262958a262537e512df90978ae3b8fc
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-27 09:57:44 -0300

    :sparkles: feat: Passo 7!

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,22 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
-
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit: function () {
+        var oData = {
+          recipient: {
+            name: "World",
+          },
+        };
+
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
       onShowHello: function () {
         MessageToast.show("Hello World");
       },
