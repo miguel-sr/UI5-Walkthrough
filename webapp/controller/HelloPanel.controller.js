81525fd93fea56fec96284945b8f075e70a9070f
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-27 14:46:20 -0300

    :sparkles: feat: Passo 16!

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
@@ -13,6 +13,18 @@ sap.ui.define(
 
         // show message
         MessageToast.show(sMsg);
+      },
+      onOpenDialog: function () {
+        // create dialog lazily
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+          });
+        }
+
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
+        });
       },
     });
   }
