74dd6cb272c4849580af93c5755b9e617a575a54
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-28 17:24:34 -0300

    :sparkles: feat: Passo 32!

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,22 +1,36 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  function (Controller, History) {
+    "use strict";
 
-  return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
-    onInit: function () {
-      var oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this._onObjectMatched, this);
-    },
-    _onObjectMatched: function (oEvent) {
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        model: "invoice",
-      });
-    },
-  });
-});
+    return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+      onInit: function () {
+        var oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
+      _onObjectMatched: function (oEvent) {
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          model: "invoice",
+        });
+      },
+      onNavBack: function () {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+    });
+  }
+);
