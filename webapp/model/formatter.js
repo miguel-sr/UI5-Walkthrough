5a6fda17abecc44e1e9675224870ebba684b5825
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-28 09:18:23 -0300

    :sparkles: feat: Passo 22!

---
+++ webapp/model/formatter.js
@@ -0,0 +1,25 @@
+sap.ui.define([], function () {
+  "use strict";
+
+  return {
+    statusText: function (sStatus) {
+      var resourceBundle = this.getOwnerComponent()
+        .getModel("i18n")
+        .getResourceBundle();
+
+      switch (sStatus) {
+        case "A":
+          return resourceBundle.getText("invoiceStatusA");
+
+        case "B":
+          return resourceBundle.getText("invoiceStatusB");
+
+        case "C":
+          return resourceBundle.getText("invoiceStatusC");
+
+        default:
+          return sStatus;
+      }
+    },
+  };
+});
