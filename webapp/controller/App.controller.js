e21ab3dd398f99c26f03111da56b94ff86a1ffa2
Miguel S Ramos <miguelsramos458@gmail.com>
2023-05-02 16:09:52 -0300

    :sparkles: feat: Iniciando desenvolvimento da funcionalidade de multi linguagem

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,19 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onInit: function () {
-      this.getView().addStyleClass(
-        this.getOwnerComponent().getContentDensityClass()
-      );
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/core/syncStyleClass",
+  ],
+  function (Controller, Fragment, syncStyleClass) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit: function () {
+        this.getView().addStyleClass(
+          this.getOwnerComponent().getContentDensityClass()
+        );
+      },
+
+      onLanguageSelectDialogPress: function () {},
+    });
+  }
+);
