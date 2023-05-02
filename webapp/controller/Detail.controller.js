4f78762e6ef88e5911177423633d2e18316962e2
Miguel S Ramos <miguelsramos458@gmail.com>
2023-05-02 10:23:43 -0300

    :sparkles: feat: Passo 33!

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
-  function (Controller, History) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+  ],
+  function (Controller, History, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
@@ -10,7 +14,9 @@ sap.ui.define(
           .getRoute("detail")
           .attachPatternMatched(this._onObjectMatched, this);
       },
+
       _onObjectMatched: function (oEvent) {
+        this.byId("rating").reset();
         this.getView().bindElement({
           path:
             "/" +
@@ -20,6 +26,7 @@ sap.ui.define(
           model: "invoice",
         });
       },
+
       onNavBack: function () {
         var oHistory = History.getInstance();
         var sPreviousHash = oHistory.getPreviousHash();
@@ -28,8 +35,19 @@ sap.ui.define(
           window.history.go(-1);
         } else {
           var oRouter = this.getOwnerComponent().getRouter();
-          oRouter.navTo("overview", {}, true);
+          // oRouter.navTo("overview", {}, true);
         }
+      },
+
+      onRatingChange: function (oEvent) {
+        var fValue = oEvent.getParameter("value");
+        var oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+
+        MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
       },
     });
   }
